a73b43ec294ad7abc5c75a3a1735a3134e5ec1f2
Marcus Rangel <marcus.vbrangel@gmail.com>
2022-04-06 13:39:56 -0300

    chore: add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-
 const sum = (number1: number, number2: number): number => {
   return number1 + number2;
-}
+};
 
 const double = (number1 = 0, number2 = 0): number => {
   return number1 * number2;
-}
+};
 
 const main = (): void => {
   const sumResult = sum(5, 10);
-  console.log('sum', sumResult);  
+
+  console.log('sum', sumResult);
+
   const doubleResult = double(5);
-  console.log('double', doubleResult);  
-}
+
+  console.log('double', doubleResult);
+};
 
 main();
