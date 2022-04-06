b0769f46d683d1fc433f21420ba74c6121a19180
Marcus Rangel <marcus.vbrangel@gmail.com>
2022-04-06 09:55:34 -0300

    chore: add typescript

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+
+const sum = (number1: number = 0, number2: number = 0): number => {
+  return number1 + number2;
+}
+
+const double = (number1: number = 0, number2: number = 0): number => {
+  return number1 * number2;
+}
+
+const main = (): void => {
+  const sumResult = sum(5, 10);
+  console.log('sum', sumResult);  
+  const doubleResult = double(5);
+  console.log('double', doubleResult);  
+}
+
+main();
